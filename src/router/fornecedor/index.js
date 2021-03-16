a2cd8c8022d5d35609abf9c9ef98ed48626ebf39
Anderson Conceicao <andersonconceiicao@gmail.com>
2021-03-16 13:25:15 -0300

    rota de create

--- src/router/fornecedor/index.js
+++ src/router/fornecedor/index.js
@@ -1,9 +1,19 @@
-const context = require('./fornecedor-model')
+const _context = require('./fornecedor-model')
 const router =require('express').Router();
 
-router.use('/', (request, response) => {
-    const result = context.findAll()
-    return response.status(200).json(result);
+router.get('/', async (request, response) => {
+    const result = await _context.findAll()
+    return response.status(200).json({
+        data: result
+    });
+})
+router.post('/', async (request, response) => {    
+    const fornecedor = request.body;
+    await _context.create(fornecedor);
+    return response.status(201).json({
+        message: 'criado com sucesso'
+    })
+
 })
 
 module.exports = router;
