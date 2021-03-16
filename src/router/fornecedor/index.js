bf2ce37318b27d3d19bdde2356a2fb32f76aa61d
Anderson Conceicao <andersonconceiicao@gmail.com>
2021-03-16 13:52:51 -0300

    filtrando por id

--- src/router/fornecedor/index.js
+++ src/router/fornecedor/index.js
@@ -1,5 +1,5 @@
 const _context = require('./fornecedor-model')
-const router =require('express').Router();
+const router = require('express').Router();
 
 router.get('/', async (request, response) => {
     const result = await _context.findAll()
@@ -7,6 +7,22 @@ router.get('/', async (request, response) => {
         data: result
     });
 })
+router.get('/:id', async (request, response) => {
+    const id = request.params.id
+
+    const query = await _context.findOne({
+        where: {
+            id: id
+        }
+    })
+    if(!query) {
+        return response.status(404).json({
+            message: 'fornecedor não encontrado!'
+        })
+    }
+    return response.status(200).json(query)
+})
+
 router.post('/', async (request, response) => {    
     const fornecedor = request.body;
     await _context.create(fornecedor);
