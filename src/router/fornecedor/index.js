e3c1abef1a5cd5aa9c10b9b676c6395265370dac
Anderson Conceicao <andersonconceiicao@gmail.com>
2021-03-17 15:18:23 -0300

    cadastrando produto e ajustando method de listar por fornecedor

--- src/router/fornecedor/index.js
+++ src/router/fornecedor/index.js
@@ -1,6 +1,8 @@
 const NotFound = require('../../errors/NotFound');
 const _context = require('./fornecedor-model')
+const _contextProduct = require('../produto/produto-model')
 const router = require('express').Router();
+/** DA PARA REFATORAR, MAS NÃO É O CASO NO MOMENTO. */
 class Repository {
     async getById(id) {
         const query = await _context.findOne({
@@ -8,13 +10,30 @@ class Repository {
                 id: id
             }
         })
-        if(query == null){
+        if (query == null) {
             console.log(query)
             throw new NotFound()
         }
         return query;
     }
+    async getProductByFornecedor(id) {
+
+        const query = await _contextProduct.findAll({
+            where: {
+                fornecedorId: id
+            }
+        })
+        console.log(query)
+
+        if (query.length === 0) {
+            throw new NotFound();
+        }
+
+        return query
+
+    }
 }
+
 const repository = new Repository()
 
 router.get('/', async (request, response) => {
@@ -47,11 +66,14 @@ router.post('/', async (request, response, next) => {
     }
 })
 
-router.get('/:id/produtos', async (request, response) => {
-    const { id } = request.params
-    return response.status(200).json({
-        message: id
-    })
+router.get('/:id/produtos', async (request, response, next) => {
+    try {
+        const { id } = request.params
+        const query = await repository.getProductByFornecedor(id);
+        return response.status(200).json(query)
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.put('/:id', async (request, response) => {
--- src/router/produto/index.js
+++ src/router/produto/index.js
@@ -1,9 +1,16 @@
 const route = require('express').Router();
+const _context = require('../produto/produto-model')
 
 route.get('/', async (request, response) => {
-    response.status(200).json({
-        data: 'hi, product!'
-    })
+    const products = await _context.findAll();
+    response.status(200).json(products);
 })
 
+route.post('/', async (request, response) => {
+    const produto = request.body;
+    console.log(produto)
+    await _context.create(produto);
+    return response.status(201).json(produto)
+
+})
 module.exports = route;
