ffc99b809f17ede9aa90cabf7137d33b828d635a
Anderson Conceicao <andersonconceiicao@gmail.com>
2021-03-17 16:09:48 -0300

    retornando por ID

--- src/router/produto/index.js
+++ src/router/produto/index.js
@@ -1,8 +1,21 @@
 const route = require('express').Router();
+const NotFound = require('../../errors/NotFound');
+const router = require('../fornecedor');
 const _context = require('../produto/produto-model')
 
 route.get('/', async (request, response) => {
     const products = await _context.findAll();
+    response.status(200).json(products);
+})
+
+
+route.get('/:id', async (request, response) => {
+    const { id } = request.params
+     const products = await _context.findOne({
+         where: { 
+             id: id
+         }
+     });
     response.status(200).json(products);
 })
 
